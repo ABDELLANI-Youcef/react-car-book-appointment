aca469e2b960b1f98bfb695b9b629bf9459d9540
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-24 17:10:01 +0100

    make a simple GET request to the server

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,26 @@
-const Home = () => (
-  <div>
-    <h1>Welcome Home</h1>
-  </div>
-);
+const simpleRequest = async () => {
+  try {
+    const options = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch('http://[::1]:3000/cars', options);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const Home = () => {
+  simpleRequest();
+  return (
+    <div>
+      <h1>Welcome Home</h1>
+    </div>
+  );
+};
 export default Home;
