a385f8c1eed2fcc8b2e0be4abc7e82896298ebd7
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-27 18:27:43 +0100

    show the appointmets of the user

--- src/containers/Appointments.js
+++ src/containers/Appointments.js
@@ -1,18 +1,38 @@
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const Appointments = ({ appointments }) => (
-  <div>
-    {appointments.map((a) => (<p key={a.id}>{a.city}</p>))}
-  </div>
-);
+const Appointments = ({ appointments, cars }) => {
+  const correspondCar = (carId) => {
+    let i = 0;
+    while (cars[i].id !== carId) {
+      i += 1;
+    }
+    return cars[i];
+  };
+  return (
+    <div>
+      {appointments.map((a) => {
+        const car = correspondCar(a.car_id);
+        return (
+          <p key={a.id}>
+            {a.city}
+            {' '}
+            {car.mark}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
 
 Appointments.propTypes = {
   appointments: PropTypes.arrayOf(PropTypes.any).isRequired,
+  cars: PropTypes.arrayOf(PropTypes.any).isRequired,
 };
 
 const mapStateToProp = (state) => ({
   appointments: state.appointment,
+  cars: state.carsList,
 });
 
 export default connect(mapStateToProp)(Appointments);
