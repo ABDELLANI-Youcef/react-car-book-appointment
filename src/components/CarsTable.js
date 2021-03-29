2db83bfa19d325d8ab48e566e7bd1134dfaf6139
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-29 18:38:57 +0100

    move the table from Home to component/CarsTable.js

---
+++ src/components/CarsTable.js
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const CarsTable = ({ cars, admin, clickHandler }) => {
+  let deleteTH = null;
+  let editTH = null;
+  const handleClick = (e) => {
+    const carId = parseInt(e.target.dataset.carId, 10);
+    clickHandler(carId);
+  };
+
+  if (admin) {
+    deleteTH = (<th>delete the car</th>);
+    editTH = (<th>edit the car</th>);
+  }
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Mark</th>
+          <th>Model</th>
+          <th>Fabrication year</th>
+          <th>Price of trying</th>
+          <th>make appointment</th>
+          {deleteTH}
+          {editTH}
+        </tr>
+      </thead>
+      <tbody>
+        {cars.map((e) => {
+          let deleteTD = null;
+          let editTD = null;
+          if (admin) {
+            deleteTD = (<td><button type="button" data-car-id={e.id} onClick={handleClick}>delete the car</button></td>);
+            editTD = (<td><Link to={{ pathname: '/edit_car', state: { car: e } }}>edit the car</Link></td>);
+          }
+          return (
+            <tr key={`${e.mark} ${e.model}`}>
+              <td>{e.mark}</td>
+              <td>{e.model}</td>
+              <td>{e.year}</td>
+              <td>{e.price}</td>
+              <td><Link to={{ pathname: '/make-appointment', state: { car: e } }}>Make appointment</Link></td>
+              {deleteTD}
+              {editTD}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+CarsTable.propTypes = {
+  admin: PropTypes.bool.isRequired,
+  cars: PropTypes.arrayOf(PropTypes.any).isRequired,
+  clickHandler: PropTypes.func.isRequired,
+};
+
+export default CarsTable;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { createCarsList } from '../actions/index';
 import { carsListRequest, deleteCarRequest } from '../logic/carRequests';
+import CarsTable from '../components/CarsTable';
 
 const Home = ({ authentication, carsList, createCarsList }) => {
   useEffect(() => {
@@ -12,56 +12,14 @@ const Home = ({ authentication, carsList, createCarsList }) => {
     }
   }, []);
 
-  const deleteCar = (e) => {
-    const carId = parseInt(e.target.dataset.carId, 10);
+  const deleteCar = (carId) => {
     deleteCarRequest(authentication.authToken, carId);
   };
 
   let carsTable = null;
-  let deleteTH = null;
-  let editTH = null;
-
   if (carsList.length > 0) {
-    if (authentication.admin) {
-      deleteTH = (<th>delete the car</th>);
-      editTH = (<th>edit the car</th>);
-    }
     carsTable = (
-      <table>
-        <thead>
-          <tr>
-            <th>Mark</th>
-            <th>Model</th>
-            <th>Fabrication year</th>
-            <th>Price of trying</th>
-            <th>make appointment</th>
-            {deleteTH}
-            {editTH}
-          </tr>
-        </thead>
-        <tbody>
-          {carsList.map((e) => {
-            let deleteTD = null;
-            let editTD = null;
-            if (authentication.admin) {
-              deleteTD = (<td><button type="button" data-car-id={e.id} onClick={deleteCar}>delete the car</button></td>);
-              editTD = (<td><Link to={{ pathname: '/edit_car', state: { car: e } }}>edit the car</Link></td>);
-            }
-            return (
-              <tr key={`${e.mark} ${e.model}`}>
-                <td>{e.mark}</td>
-                <td>{e.model}</td>
-                <td>{e.year}</td>
-                <td>{e.price}</td>
-                <td><Link to={{ pathname: '/make-appointment', state: { car: e } }}>Make appointment</Link></td>
-                {deleteTD}
-                {editTD}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-
+      <CarsTable admin={authentication.admin} cars={carsList} clickHandler={deleteCar} />
     );
   }
 
