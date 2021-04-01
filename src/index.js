1bcc83184ebc45e0a672c3b7e29b93d1990f64a9
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-04-01 13:58:11 +0100

    add redux-persist to the project

--- src/index.js
+++ src/index.js
@@ -2,25 +2,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import Router from './Router';
 import authenticationReducer from './reducers/authentication';
 import carsListReducer from './reducers/car';
 import appointmentsReducer from './reducers/appointment';
 
-const rootReducer = combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['authentication'],
+};
+
+const combinedReducer = combineReducers({
   authentication: authenticationReducer,
   carsList: carsListReducer,
   appointment: appointmentsReducer,
 });
 
+const rootReducer = persistReducer(persistConfig, combinedReducer);
 /* eslint-disable no-underscore-dangle */
 const store = createStore(rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-/* eslint-enable */
+  /* eslint-enable */
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router />
+    <PersistGate persistor={persistor}>
+      <Router />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
