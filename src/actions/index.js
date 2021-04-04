a664d43424cf4ef58b0611024299551ad0aee9a9
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-04-04 23:27:28 +0100

    add the setAppointments and removeAppointments

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ const CREATE_TOKEN = 'CREATE_TOKEN';
 const REMOVE_TOKEN = 'REMOVE_TOKEN';
 const CREATE_CARS_LIST = 'CREATE_CARS_LIST';
 const CREATE_APPOINTMENTS_LIST = 'CREATE_APPOINTMENTS_LIST';
-const ADD_APPOINTMENT = 'ADD_APPOINTMENT';
+const SET_APPOINTMENT = 'SET_APPOINTMENT';
+const REMOVE_APPOINTMENT = 'REMOVE_APPOINTMENT';
 
 const createToken = (token) => ({
   type: CREATE_TOKEN,
@@ -23,12 +24,18 @@ const createAppointmentsList = (list) => ({
   payload: list,
 });
 
-const addAppointment = (appointment) => ({
-  type: ADD_APPOINTMENT,
+const setAppointment = (appointment) => ({
+  type: SET_APPOINTMENT,
   payload: appointment,
+});
+
+const removeAppointment = (id) => ({
+  type: REMOVE_APPOINTMENT,
+  id,
 });
 
 export {
   CREATE_TOKEN, createToken, CREATE_CARS_LIST, createCarsList, REMOVE_TOKEN, removeToken,
-  CREATE_APPOINTMENTS_LIST, createAppointmentsList, ADD_APPOINTMENT, addAppointment,
+  CREATE_APPOINTMENTS_LIST, createAppointmentsList, setAppointment,
+  SET_APPOINTMENT, REMOVE_APPOINTMENT, removeAppointment,
 };
--- src/reducers/appointment.js
+++ src/reducers/appointment.js
@@ -1,4 +1,7 @@
-import { CREATE_APPOINTMENTS_LIST } from '../actions/index';
+import {
+  CREATE_APPOINTMENTS_LIST,
+  SET_APPOINTMENT, REMOVE_APPOINTMENT,
+} from '../actions/index';
 
 const defaultState = { appointments: [], size: -1 };
 
@@ -6,6 +9,11 @@ const appointmentsReducer = (state = defaultState, action) => {
   switch (action.type) {
     case CREATE_APPOINTMENTS_LIST:
       return action.payload;
+
+    case SET_APPOINTMENT:
+      return [...state, action.appointment];
+    case REMOVE_APPOINTMENT:
+      return state.filter((id) => action.id !== id);
 
     default:
       return state;
